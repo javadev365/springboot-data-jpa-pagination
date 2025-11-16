4b3382dfe7a79dc8af0d94430abb209c9215ded6
Ravi Chalamcharla <ravi.chalamcharla@wipro.com>
2025-11-16 10:52:17 +0100

    initial project setup

---
+++ src/main/java/com/javadev365/apps/pagination/SpringbootDataJpaPaginationApplication.java
@@ -0,0 +1,13 @@
+package com.javadev365.apps.pagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDataJpaPaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDataJpaPaginationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javadev365/apps/pagination/SpringbootDataJpaPaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadev365.apps.pagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDataJpaPaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
